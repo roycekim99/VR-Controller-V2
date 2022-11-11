b6cb7997a1ff021c6e1d0bc5e3bfd7eb32b8b1c2
Royce-Work Computer <roycekim99@users.noreply.github.com>
2022-11-10 21:22:29 -0800

    UDP Failing

--- server/server.js
+++ server/server.js
@@ -1,11 +1,18 @@
 const path = require("path");
 const express = require("express");
-// const { v4 } = require("uuid");
+const dgram = require("dgram");
+const server = dgram.createSocket("udp4");
+const os = require("os");
 
-/** Make Bundled Server Handler*/
-/** - Higher level class: responsible for starting and stopping server */
+const getLocalExternalIP = () =>
+  []
+    .concat(...Object.values(os.networkInterfaces()))
+    .filter((details) => details.family === "IPv4" && !details.internal)
+    .pop().address;
 
 // =============================
+const LOCAL_IP = getLocalExternalIP();
+const MULTICAST_ADDR = "239.255.255.250";
 const PORT = 3000;
 const expressApp = express();
 const httpServer = require("http").createServer(expressApp);
@@ -13,10 +20,26 @@ const io = require("socket.io")(httpServer);
 
 let users = {}; // {uid: socket}
 
+//#region UDP SERVER
+server.bind(PORT + 20, function () {
+  server.setBroadcast(true);
+  server.setMulticastTTL(128);
+  server.addMembership(MULTICAST_ADDR);
+});
+setInterval(sendMyIP, 300);
+
+function sendMyIP() {
+  server.send(LOCAL_IP, PORT, MULTICAST_ADDR);
+  console.log("Sent UPD multicast: ", LOCAL_IP);
+}
+//#endregion
+
 //#region SOCKET CALLS
 io.on("connect", (socket) => {
+  let incomingIP = socket.request.connection.address().address;
   //DEBUG
   console.info("Message received from " + socket.id);
+  console.info("Incoming IP: ", incomingIP);
 
   // ===[ SOCKET CALL FUNCTIONS]====
   socket.on("handshake", () => {
@@ -34,6 +57,7 @@ io.on("connect", (socket) => {
 //#endregion
 
 //#region API CALLS
+
 // Have Node serve the files for React app
 expressApp.use(express.static(path.resolve(__dirname, "../build")));
 
